995e1be829a386bdba60ad8189c4db751c258633
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2022-04-12 19:36:44 +0000

    listar

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -1,6 +1,6 @@
 package application.controllers;
 
-import org.sprinframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
